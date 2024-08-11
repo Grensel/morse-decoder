b7aa2f5b1608b22a04051a1c15b5418664f57e76
Grensel <sergeiodinochenko@mail.ru>
2024-08-11 15:52:29 +0300

    feat: completed an algorithmic task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,20 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let array = [];
+    let str = expr;
+    for (let i = 0; i < expr.length; i ) {
+        let letter = str.slice(i , i += 10).replace(/10/g, '.').replace(/11/g, '-').replace(/\*/g, ' ').replace(/          /, ' ').split('00').filter(Boolean);
+        let string = '';
+        if (letter == ' ') {
+            string = string + ' ';
+        } else {
+            string = string + MORSE_TABLE[letter];
+        }
+        array.push(string);
+    }
+    str = array.join('');
+    return str;
 }
 
 module.exports = {
